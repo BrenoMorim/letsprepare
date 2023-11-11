c4c9816d74ba7afd425bb2f98bc255d4f50a2c85
Breno Morim <brenomorim13@gmail.com>
2023-11-11 07:34:41 -0300

    Adding animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useCallback, useState } from 'react';
 import './App.css';
 
+const GOOD = 12;
+const OK = 6;
+
 function App() {
 
   const [theme, setTheme] = useState('');
@@ -8,25 +11,25 @@ function App() {
   const [items, setItems] = useState([]);
 
   const getColor = useCallback(() => {
-    if (items.length >= 10) return 'green';
-    if (items.length >= 5) return 'orange';
+    if (items.length >= GOOD) return 'green';
+    if (items.length >= OK) return 'orange';
     return 'red';
   }, [items]);
 
   const getImage = useCallback(() => {
-    if (items.length >= 10) return 'https://assets.epuzzle.info/puzzle/079/716/original.jpg';
-    if (items.length >= 5) return 'https://us-tuna-sounds-images.voicemod.net/31c316d4-2f90-4e7c-bca1-dfb899f7023d-1685558713339.png';
+    if (items.length >= GOOD) return 'https://assets.epuzzle.info/puzzle/079/716/original.jpg';
+    if (items.length >= OK) return 'https://us-tuna-sounds-images.voicemod.net/31c316d4-2f90-4e7c-bca1-dfb899f7023d-1685558713339.png';
     return 'https://cdn.pnghd.pics/data/893/screaming-cat-meme-1.jpg';
   }, [items]);
 
   return (
     <div className="App">
-      <header className="app__header">
+      <header className="app__header animate__animated animate__slideInDown">
         <h1>Let's organize / prepare a(n): </h1>
         <input value={theme} className='app__input form-control' onChange={(e) => setTheme(e.target.value)}/>
       </header>
       <main className="app__main">
-        <form className="app__form" onSubmit={(e) => {
+        <form className="app__form animate__animated animate__slideInLeft" onSubmit={(e) => {
             e.preventDefault();
             if (adding === '') return;
             if (items.includes(adding)) return;
@@ -39,14 +42,14 @@ function App() {
           <button className='app__btn app__btn--reverse btn' onClick={() => setItems([])}>Clear list</button>
         </form>
         <div className='app__grid'>
-          <div className='left'>
+          <div className='left animate__animated animate__slideInLeft'>
             <div className='app__items'>
               {items.map(item => (
-                <span className='app__item'>{ item }</span>
+                <span className='app__item animate__animated animate__slideInLeft'>{ item }</span>
               ))}
             </div>
           </div>
-          <div className='right'>
+          <div className='right animate__animated animate__slideInRight'>
             <span className='app__total' style={{
               color: getColor()
             }}>Total items in the list: { items.length }</span>
